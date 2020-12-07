5452ff3537d412c1cf542e3fa216b952f6a523a2
Fumito Fujiwara <fumito80@gmail.com>
2020-12-08 00:59:04 +0900

    dist moves to docs

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     index: './ts/index.ts',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: "bundle.js"
   },
   module: {
